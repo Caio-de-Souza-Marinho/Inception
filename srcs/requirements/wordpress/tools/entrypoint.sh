7b37e081c1f431e42b778305a0301602f661733e
Caio-de-Souza-Marinho <caiosouzamarinho@gmail.com>
2026-03-04 01:16:15 -0300

    wordpress setup complete

--- srcs/requirements/wordpress/tools/entrypoint.sh
+++ srcs/requirements/wordpress/tools/entrypoint.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-MYQL_PASSWORD=$(cat /run/secrets/db_password)
+MYSQL_PASSWORD=$(cat /run/secrets/db_password)
 WP_ADMIN_PASSWORD=$(cat /run/secrets/wp_admin_password)
 WP_USER_PASSWORD=$(cat /run/secrets/wp_user_password)
 
 # Wait for MariaDB to be ready
-until mysqladmin ping -h mariadb -u "${MYSQL_USER}" -p "${MYSQL_PASSWORD}" --silent; do
+until mysqladmin ping -h mariadb -u "${MYSQL_USER}" -p"${MYSQL_PASSWORD}" --silent; do
 	echo "Waiting for MariaDB..."
 	sleep 2
 done
@@ -29,14 +29,14 @@ if [ ! -f /var/www/html/wp-config.php ]; then
 		--allow-root \
 		--url="https://${DOMAIN_NAME}" \
 		--title="Inception" \
-		--admin-user="${WP_ADMIN_USER}" \
-		--admin-password="${WP_ADMIN_PASSWORD}" \
-		--admin-email="${WP_ADMIN_EMAIL}" \
+		--admin_user="${WP_ADMIN_USER}" \
+		--admin_password="${WP_ADMIN_PASSWORD}" \
+		--admin_email="${WP_ADMIN_EMAIL}" \
 
 	# Create a second regular user
 	wp user create \
 		--allow-root \
-		"${WP_USER}" "${WP_EMAIL}" \
+		"${WP_USER}" "${WP_USER_EMAIL}" \
 		--user_pass="${WP_ADMIN_PASSWORD}" \
 		--role=author
 
