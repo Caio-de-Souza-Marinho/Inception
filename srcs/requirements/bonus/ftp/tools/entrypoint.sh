386bc666898c631f359eb4091adb8a9ed914fac6
Caio-de-Souza-Marinho <caiosouzamarinho@gmail.com>
2026-03-08 00:53:25 -0300

    finish ftp container

--- srcs/requirements/bonus/ftp/tools/entrypoint.sh
+++ srcs/requirements/bonus/ftp/tools/entrypoint.sh
@@ -4,16 +4,15 @@ set -e
 
 FTP_PASSWORD=$(cat /run/secrets/ftp_password)
 
-chown -R "${FTP_USER}:${FTP_USER}" /var/www/html
+# Create required vsftp directory
+mkdir -p /var/run/vsftpd/empty
 
 # Create FTP user if it doesn't exist
 if ! id "${FTP_USER}" &>/dev/null; then
 	useradd -m -d /var/www/html "${FTP_USER}"
 	echo "${FTP_USER}:${FTP_PASSWORD}" | chpasswd
+	chown -R "${FTP_USER}:${FTP_USER}" /var/www/html
 fi
-
-# Create required vsftp directory
-mkdir -p /var/run/vsftp/empty
 
 echo "Starting FTP server..."
 exec vsftpd /etc/vsftpd.conf
